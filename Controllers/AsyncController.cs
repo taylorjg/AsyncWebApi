d891df2c54b9c12a6c6c5767942df55bcee07b3e
Jonathan Taylor <jont@cakesolutions.net>
2018-01-08 09:36:29 +0000

    Initial commit

---
+++ Controllers/AsyncController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AsyncWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class AsyncController : Controller
+    {
+        [HttpGet()]
+        public async Task<IEnumerable<string>> Get()
+        {
+            await Task.Delay(2000);
+            return new string[] { "value5", "value6" };
+        }
+    }
+}
